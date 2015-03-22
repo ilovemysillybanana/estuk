f2e18183374e855735eb84a86ee134e36decf64d
Jose Tobar <ilovemysillybanana@gmail.com>
2015-03-22 02:57:49 -0400

    Added Sales model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ActiveRecord::Base
   friendly_id :name, use: :slugged
 
   belongs_to :user
+  has_many :sales
   has_attached_file :image
   has_attached_file :resource
 
@@ -12,6 +13,10 @@ class Book < ActiveRecord::Base
   validates_attachment_content_type :resource,
       content_type: ['application/pdf'],
       message: "Only pdf's allowed"
+
   validates :image, attachment_presence: true
   validates :resource, attachment_presence: true
+
+  validates_numericality_of :price,
+      greater_than: 49, message: "Must be at least 50 cents"
 end
