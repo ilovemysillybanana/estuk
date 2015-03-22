dfe5a8c15b9f9f17302ef466ec9cb2e94a8f7e9d
Jose Tobar <ilovemysillybanana@gmail.com>
2015-03-22 02:43:07 -0400

    Images now showing up dynamically

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,8 +27,16 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = current_user.books.new(book_params)
-    @book.save
-    respond_with(@book)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /books/1
