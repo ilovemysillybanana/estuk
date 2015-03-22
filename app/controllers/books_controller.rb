4c156d9bb2e8b30dfed5e96d274da6006b50db5f
Jose Tobar <ilovemysillybanana@gmail.com>
2015-03-22 17:23:50 -0400

    Test One

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,56 +2,77 @@ class BooksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
-
+  # GET /books
+  # GET /books.json
   def index
-    if params[:search]
-      @books = Book.search(params[:search],params[:author], params[:min_price], params[:max_price]).available
-    else
-      @books = current_user.books
-    end
-
-    respond_with(@books)
+    @books = Book.where(availability: true)
   end
 
+  # GET /books/1
+  # GET /books/1.json
   def show
-    respond_with(@book)
   end
 
+  # GET /books/new
   def new
     @book = Book.new
-    respond_with(@book)
   end
 
+  # GET /books/1/edit
   def edit
     authorize! :manage, @book
   end
 
+  # POST /books
+  # POST /books.json
   def create
     @book = current_user.books.new(book_params)
-    @book.save
-    respond_with(@book)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /books/1
+  # PATCH/PUT /books/1.json
   def update
     authorize! :manage, @book
-    @book.update(book_params)
-    respond_with(@book)
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /books/1
+  # DELETE /books/1.json
   def destroy
     authorize! :manage, @book
     @book.destroy
-    respond_with(@book)
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.friendly.find(params[:id])
+    end
 
-  def set_book
-    @book = Book.friendly.find(params[:id])
-  end
-
-  def book_params
-    params.require(:book).permit(:name, :author, :description, :price, :availability, :image, :resource)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def book_params
+      params.require(:book).permit(:name, :author, :description, :price, :availability, :image, :resource)
+    end
 end
