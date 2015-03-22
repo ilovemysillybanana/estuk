ba8c113b5bbd7d80acaf3ec815a64d5a2eff9ac6
Jose Tobar <ilovemysillybanana@gmail.com>
2015-03-22 02:12:17 -0400

    AWS is now configured with paperclip

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,16 +27,8 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = current_user.books.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
+    @book.save
+    respond_with(@book)
   end
 
   # PATCH/PUT /books/1
